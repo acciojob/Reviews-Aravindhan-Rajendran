b025ee4cbae876d4e327f516bc680a1583114b3f
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-26 20:44:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,15 @@
-import React from react;
-const person=[
-  {
-    id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-  },
-  {
-    id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-    text:
-      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
-  },
-  {
-    id: 3,
-    name: 'peter jones',
-    job: 'intern',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-    text:
-      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
-  },
-  {
-    id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-    text:
-      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
-  },
-];
-const App=()=>{
-<h1 id="review-heading">Our Reviews</h1>
-  persons.forEach((item)=>{
-<div className="review">
-  <p className="author-id">{item.id}</p>
-  <p className="author">{item.name}</p>
-  <p className="job">{item.job}</p>
-  <p className="info">{item.text}</p>
-  <p className="person-img">{item.image}</p>
-                  <button type="button" className="prev-btn">Previous</button>
-                  <button type="button" className="next-btn">Next</button>
-                  <button type="button" className="random-btn">suprise me</button>
-  <div>
-}});
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+import Review from './Review'; // Import the Review component
 
+const App = () => {
+  return (
+    <main>
+      <section className="container">
+        <h1 id="review-heading">Our Reviews</h1>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
